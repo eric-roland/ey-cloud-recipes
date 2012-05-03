235ed9020a944e278295d0468069fa917ed6e0e5
eric-roland <eric.roland@gmail.com>
2012-05-03 10:46:38 -0400

    adds ffmpeg

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -23,3 +23,12 @@
 #   version "2.7.6"
 #   action :install
 # end
+
+enable_package "media-video/ffmpeg" do
+  version "0.4.9_p20090201"
+end
+
+package "media-video/ffmpeg" do
+  version "0.4.9_p20090201"
+  action :install
+end
